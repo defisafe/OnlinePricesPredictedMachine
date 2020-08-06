50c1f996909e41c8bcd11c0fdac3e03662ca467a
木生 <455194872@qq.com>
2020-08-06 18:28:02 +0800

    Price Forecast version 2.0(#13) * Start building a new version of the prophecy

--- contracts/OnlinePricesPredictedMachine.sol
+++ contracts/OnlinePricesPredictedMachine.sol
@@ -1,25 +1,102 @@
 pragma solidity 0.6.0;
-import "./KyberSwapFactory.sol";
 import "./SafeMath.sol";
+import "./ERC20Interface.sol";
 
+interface AggregatorInterface {
+  function latestAnswer() external view returns (int256);
+  function latestTimestamp() external view returns (uint256);
+  function latestRound() external view returns (uint256);
+  function getAnswer(uint256 roundId) external view returns (int256);
+  function getTimestamp(uint256 roundId) external view returns (uint256);
 
+  event AnswerUpdated(int256 indexed current, uint256 indexed roundId, uint256 updatedAt);
+  event NewRound(uint256 indexed roundId, address indexed startedBy, uint256 startedAt);
+}
 
 contract OnlinePricesPredictedMachine {
 
     using SafeMath for uint256;
+    address payable owner;
+    
+    struct PriceFeedStruct{
+        uint256 feedAddressTotal;
+        uint256 tokenID_Total;
+        address daiToETHFeedAddress;
+        mapping(uint256 => address) feedAddressManager;
+        mapping(address => uint256) tokenID_Manager;
+    }
+    
+    PriceFeedStruct public tokenPriceFeedStruct;
+    
+    constructor() public {
+        tokenPriceFeedStruct = PriceFeedStruct({feedAddressTotal:0,tokenID_Total: 0});
+        owner = msg.sender;
+    }
+    
+     modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
 
-    constructor() public {}
+    function setDaiToETHFeedAddress(address feedAddress) public onlyOwner{
+        
+        require(feedAddress != address(0),"daiToETHFeedAddress error .");
+        tokenPriceFeedStruct.daiToETHFeedAddress = feedAddress;
+
+    }
+    
+    function setPriceFeedAddress(uint256 tokenID,address feedAddress) public onlyOwner{
+        
+        require(tokenID == tokenPriceFeedStruct.feedAddressTotal,"feedAddressTotal error .");
+        tokenPriceFeedStruct.feedAddressManager(tokenID) = feedAddress;
+        tokenPriceFeedStruct.feedAddressTotal = tokenPriceFeedStruct.feedAddressTotal+1;
+        
+    }
+    
+     function setTokenIDAddress(address tokenAddress,uint256 tokenID) public onlyOwner{
+        
+        require(tokenID == tokenPriceFeedStruct.tokenID_Total,"tokenID_Total error .");
+        tokenPriceFeedStruct.tokenID_Manager(tokenAddress) = tokenID;
+        tokenPriceFeedStruct.tokenID_Total = tokenPriceFeedStruct.tokenID_Total+1;
+        
+    }
+    
+    
 
     function getPriceOfDAI(ERC20 srcToken,uint256 amount) public returns(uint256){
-        return  KyberSwapFactory.getPriceOfDAI(srcToken,amount);
+        
+        uint256 tokenID = tokenPriceFeedStruct.tokenID_Manager[address(srcToken)];
+        address feedAddress = tokenPriceFeedStruct.feedAddressManager[tokenID];
+        AggregatorInterface feedManager = AggregatorInterface(feedAddress);
+        uint256 priceSrcTokenToEth = feedManager.latestAnswer;
+        uint256 getPriceEthToDai = getPriceEthToDai(0);
+        return mulDiv(priceSrcTokenToEth,getPriceEthToDai,1e18);
     }
 
      function getPriceEthToDai(uint256 amount) public view returns(uint,uint){
-         return KyberSwapFactory.getEthToDaiPrice(amount);
+         
+         require(tokenPriceFeedStruct.daiToETHFeedAddress != address(0),"no set daiToETHFeedAddress");
+         AggregatorInterface feedManager = AggregatorInterface(tokenPriceFeedStruct.daiToETHFeedAddress);
+         uint256 priceDaiToETH = feedManager.latestAnswer;
+         return mulDiv(1e18,1e18,priceDaiToETH);
      }
 
+
     function getPriceTokenToToken(ERC20 srcToken,ERC20 destToken,uint256 amount) public returns(uint,uint){
-        return KyberSwapFactory.getPrice(srcToken,destToken,amount);
+        
+        uint256 tokenIDSrcToken = tokenPriceFeedStruct.tokenID_Manager[address(srcToken)];
+        address feedAddressSrcToken = tokenPriceFeedStruct.feedAddressManager[tokenIDSrcToken];
+        AggregatorInterface feedManagerSrcToken = AggregatorInterface(feedAddressSrcToken);
+        uint256 priceSrcTokenToEth = feedManagerSrcToken.latestAnswer;
+        
+        uint256 tokenIDDestToken = tokenPriceFeedStruct.tokenID_Manager[address(destToken)];
+        address feedAddressDestToken = tokenPriceFeedStruct.feedAddressManager[tokenIDDestToken];
+        AggregatorInterface feedManagerDestToken = AggregatorInterface(feedAddressDestToken);
+        uint256 priceDestTokenToEth = feedManagerDestToken.latestAnswer;
+        
+        uint256 getPriceEthToDestToken = mulDiv(1e18,1e18,priceDestTokenToEth);
+        
+        return mulDiv(priceSrcTokenToEth,getPriceEthToDestToken,1e18);
     }
 
     function swapEthToToken(ERC20 token, address destAddress) public returns(uint256){
@@ -39,33 +116,12 @@ contract OnlinePricesPredictedMachine {
         return KyberSwapFactory.execSwapTokenToToken(srcToken,srcQty,destToken,destAddress);
     }
 
-    function slipPriceProtectionOfDai(ERC20 srcToken,uint256 amount)public returns(uint256,uint256){
-
-        uint256 standardPrice = getPriceOfDAI(srcToken,mulDiv(amount,1,100));
-        uint256 actualPrice = getPriceOfDAI(srcToken,amount);
-
-        uint256 errorType = 0;
-        if(standardPrice == 0){
-            errorType = 10;
-        }else if(actualPrice == 0){
-            errorType = 11;
-        }else if(actualPrice > standardPrice){
-            errorType = 12;
-        }
-        uint256 actualSlip = standardPrice.sub(actualPrice); 
-        uint256 maxSlip = mulDiv(standardPrice,5,100);
-        if(actualSlip > maxSlip){
-            errorType = 13;
-        }
-        return (errorType,actualPrice);
-    }
-
 
     // Receive ETH
     fallback() external payable {}
     receive() external payable {}
 
-     function mulDiv (uint256 _x, uint256 _y, uint256 _z) public pure returns (uint256) {
+    function mulDiv (uint256 _x, uint256 _y, uint256 _z) public pure returns (uint256) {
         uint256 temp = _x.mul(_y);
         return temp.div(_z);
     }
